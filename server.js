6a36276730c26de796ed642db5b28132e8a8cace
mtorcellini <matt.torcellini@gmail.com>
2020-11-25 13:14:59 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgersController");
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+// Serve static files (css and js);
+app.use(express.static("public"));
+
+// Set up handlebars
+app.engine("handlebars", exphbs({defaultLayout : "main"}));
+app.set("view engine", "handlebars");
+
+// Give server acceess to burgersController
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log('Server is listening on port ' + PORT);
+});
+
