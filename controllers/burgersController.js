4b57c712628dc28078d310bf3c4fb441ac574e8f
mtorcellini <matt.torcellini@gmail.com>
2020-11-26 00:20:31 -0500

    select all function

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const burger = require("../models/burger");
 
 
 // Get all the burgers
 router.get("/", (req, res, next) => {
-
+    burger.all((data) => {
+        console.log("Burger controller got this data: ", data);
+        res.render("index", {
+            burgersToRender : data
+        })
+    })
 });
 
 
 // Create a new burger
 router.post("/api/burgers", (req, res, next) => {
-
+    burger.create()
 });
 
 
 // Update the state of a burger
 router.put("/api/burgers/:id", (req, res, next) => {
-    
-})
 
+});
+
+module.exports = router;
